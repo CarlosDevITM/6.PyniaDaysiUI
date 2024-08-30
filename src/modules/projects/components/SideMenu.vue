40bbdf3cb827140a5e1d943ec14be25d82c016a6
CarlosDevITM <carlos.ponce@devitm.com>
2024-08-29 19:29:43 -0600

    Se realizó la estructura del proyecto de banco de proyectos

---
+++ src/modules/projects/components/SideMenu.vue
@@ -0,0 +1,33 @@
+<template>
+  <!--                   COLOR        WIDTH   HEIGHT-->
+  <aside class="bg-base-200 w-72 min-h-screen">
+    <!--             Text large  BOLD   margin l and r -->
+    <h2 class="text-lg font-bold mx-4">Proyectos</h2>
+    <!--              Small text    Text color   margin l and r-->
+    <p class="text-sm text-gray-500 mx-4">No hay proyectos</p>
+
+    <!--Daisy UI Side Menu-->
+    <ul class="menu">
+      <li><a>Item 1</a></li>
+      <li>
+        <details open>
+          <summary>Parent</summary>
+          <ul>
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+            <li>
+              <details open>
+                <summary>Parent</summary>
+                <ul>
+                  <li><a>Submenu 1</a></li>
+                  <li><a>Submenu 2</a></li>
+                </ul>
+              </details>
+            </li>
+          </ul>
+        </details>
+      </li>
+      <li><a>Item 3</a></li>
+    </ul>
+  </aside>
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,14 @@
+import ProjectsLayout from '@/modules/projects/layouts/ProjectsLayout.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'projects',
+      component: ProjectsLayout,
+    },
+  ],
 });
 
 export default router;
