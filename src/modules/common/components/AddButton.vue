5b03f09e1ff95139d8bf9bc56397b322e6b5ff8e
CarlosDevITM <carlos.ponce@devitm.com>
2024-08-30 14:35:06 -0600

    Se modifico un boton, agregandole un slot para simplificar el código y reutilizar el ícono

---
+++ src/modules/common/components/AddButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <!-- Float button from DaisyUI-->
+  <button @click="$emit('click')" :class="['btn btn-circle fixed', position]">
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts" setup>
+//Enviar cada que se hace click al boton.
+defineEmits(['click']);
+//Icono
+//Crear una interfaz con las posibles ubicaciones del boton.
+interface Props {
+  position?: 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right';
+}
+//Definir las propiedades con una propiedad por defecto
+withDefaults(defineProps<Props>(), {
+  position: 'bottom-right',
+});
+</script>
+
+<style>
+.bottom-right {
+  @apply bottom-10 right-10;
+}
+.top-left {
+  @apply top-10 left-10;
+}
+.top-right {
+  @apply top-10 right-10;
+}
+.bottom-left {
+  @apply bottom-10 left-10;
+}
+</style>
--- src/modules/projects/pages/ProjectsPage.vue
+++ src/modules/projects/pages/ProjectsPage.vue
@@ -21,22 +21,14 @@
         </tr>
       </tbody>
     </table>
-
-    <!-- Float button from DaisyUI-->
-    <button class="btn btn-circle fixed bottom-10 right-10">
-      <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-        <g id="SVGRepo_iconCarrier">
-          <circle cx="12" cy="12" r="10" stroke="#ffffff" stroke-width="1.5"></circle>
-          <path
-            d="M15 12L12 12M12 12L9 12M12 12L12 9M12 12L12 15"
-            stroke="#ffffff"
-            stroke-width="1.5"
-            stroke-linecap="round"
-          ></path>
-        </g>
-      </svg>
-    </button>
   </div>
+  <AddButton position="bottom-right" @click="() => console.log('clicked')">
+    <addIcon></addIcon>
+  </AddButton>
 </template>
+
+<script lang="ts" setup>
+import AddButton from '@/modules/common/components/AddButton.vue';
+
+import addIcon from '@/modules/common/components/icons/addIcon.vue';
+</script>
