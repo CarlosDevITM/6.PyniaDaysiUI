ffe5d78b14a4120e209bb51ce33d2bb97c9fabfe
CarlosDevITM <carlos.ponce@devitm.com>
2024-09-03 14:12:02 -0600

    Se controló la redirección de las rutas y se creo una tabla para agregar tareas

--- src/modules/projects/pages/ProjectDetails.vue
+++ src/modules/projects/pages/ProjectDetails.vue
@@ -1,5 +1,39 @@
 <template>
-  <NavBreadCrumbs :name="project?.name ?? 'No name'"></NavBreadCrumbs>
+  <div class="w-full">
+    <section class="m-2">
+      <NavBreadCrumbs :name="project?.name ?? 'No name'"></NavBreadCrumbs>
+    </section>
+
+    <!-- table-->
+    <section>
+      <div class="overflow-x-auto">
+        <table class="table">
+          <!-- head -->
+          <thead>
+            <tr>
+              <th class="w-14">#</th>
+              <th>Tarea</th>
+              <th>Completada</th>
+            </tr>
+          </thead>
+          <tbody>
+            <!-- row 2 -->
+            <tr class="hover">
+              <td>Si</td>
+              <td>
+                <input
+                  type="text"
+                  class="input input-primary w-full opacity-60 transition-all"
+                  placeholder="Ingresa una nueva tarea"
+                />
+              </td>
+              <td>Completada</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -7,6 +41,7 @@ import NavBreadCrumbs from '@/modules/common/components/NavBreadCrumbs.vue';
 import { ref, watch } from 'vue';
 import { useProjectStore } from '../stores/projectStore';
 import type { Project } from '../interfaces/IProjects';
+import router from '@/router';
 
 interface Props {
   id: string;
@@ -22,6 +57,9 @@ watch(
   () => props.id,
   () => {
     project.value = projectStore.projectList.find((project) => project.id === props.id);
+    if (!project.value) {
+      router.replace('/');
+    }
   },
   //Ejecutar tan pronto en componente se monte
   {
