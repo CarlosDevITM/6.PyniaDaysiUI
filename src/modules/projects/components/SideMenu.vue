ec0f8c54f74572cd617ccc9eda2868c4066f1c3c
CarlosDevITM <carlos.ponce@devitm.com>
2024-09-03 09:17:15 -0600

    Se guardaron las tareas en el localstorage y se configuró el sideMenu para mostrar los proyectos

--- src/modules/projects/components/SideMenu.vue
+++ src/modules/projects/components/SideMenu.vue
@@ -4,30 +4,42 @@
     <!--             Text large  BOLD   margin l and r -->
     <h2 class="text-lg font-bold mx-4">Proyectos</h2>
     <!--              Small text    Text color   margin l and r-->
-    <p class="text-sm text-gray-500 mx-4">No hay proyectos</p>
+    <!-- IF there´s no projects-->
+    <p v-if="projectStore.noProjects" class="text-sm text-gray-500 mx-4">No hay proyectos</p>
 
     <!--Daisy UI Side Menu-->
-    <ul class="menu">
-      <li><a>Item 1</a></li>
-      <li>
-        <details open>
-          <summary>Parent</summary>
-          <ul>
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-            <li>
-              <details open>
-                <summary>Parent</summary>
-                <ul>
-                  <li><a>Submenu 1</a></li>
-                  <li><a>Submenu 2</a></li>
-                </ul>
-              </details>
-            </li>
-          </ul>
-        </details>
+    <!-- If there are-->
+    <ul v-else class="menu">
+      <li v-for="project in projectStore.projectList" :key="project.id">
+        <template v-if="project.tasks.length > 0">
+          <details>
+            <!-- Project's name if there´s tasks.-->
+            <summary>
+              <RouterLink :to="`/project/${project.id}`">
+                {{ project.name }}
+              </RouterLink>
+            </summary>
+            <ul>
+              <li v-for="task in project.tasks" :key="task.id">
+                <RouterLink :to="`/project/${project.id}`">{{ task.name }}</RouterLink>
+              </li>
+            </ul>
+          </details>
+        </template>
+
+        <!-- If there´s no tasks-->
+        <template v-else>
+          <RouterLink :to="`/project/${project.id}`">
+            {{ project.name }}
+          </RouterLink>
+        </template>
       </li>
-      <li><a>Item 3</a></li>
     </ul>
   </aside>
 </template>
+
+<script lang="ts" setup>
+import { useProjectStore } from '../stores/projectStore';
+
+const projectStore = useProjectStore();
+</script>
--- src/modules/projects/stores/projectStore.ts
+++ src/modules/projects/stores/projectStore.ts
@@ -2,27 +2,12 @@ import { defineStore } from 'pinia';
 import { computed, ref } from 'vue';
 import type { Project } from '../interfaces/IProjects';
 import { v4 as uuidv4 } from 'uuid';
-
-//Array de prueba
-const initialLoad = (): Project[] => {
-  return [
-    {
-      id: uuidv4(),
-      name: 'Project 1',
-      tasks: [],
-    },
-
-    {
-      id: uuidv4(),
-      name: 'Project 2',
-      tasks: [],
-    },
-  ];
-};
+import { useLocalStorage } from '@vueuse/core';
 
 //Funcion de pinia
 export const useProjectStore = defineStore('projects', () => {
-  const projects = ref<Project[]>(initialLoad());
+  //Local storage
+  const projects = ref(useLocalStorage<Project[]>('projects', []));
 
   const addProject = (name: string) => {
     if (name.length === 0) return;
@@ -36,10 +21,11 @@ export const useProjectStore = defineStore('projects', () => {
 
   return {
     //Properties
-
+    projects,
     //Computed
     //Computed constant to send the projects value that means all array elements
     projectList: computed(() => [...projects.value]),
+    noProjects: computed(() => projects.value.length === 0),
 
     //Functions
     addProject,
